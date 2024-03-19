98fa67a75103ae426b2fb703de89cb16c7cf40c1
Грицина Максим <bob217@mail.ru>
2024-03-19 22:12:12 +0700

    Task-102 Добавили коэффициент

--- JuniorFactory.Lesson3/Program.cs
+++ JuniorFactory.Lesson3/Program.cs
@@ -4,5 +4,6 @@ Console.WriteLine("Hello, World!");
 var a = 1;
 var b = 2;
 var d = -1; // очень важная штука
-var c = a + b + d;
+var e = 5; // очень важный метеокоэфициент
+var c = a + b + d + e;
 Console.WriteLine(c);
