3b5dbc19d27ace2d3ee8c5872c04afa5cb227af3
Gritsina Maksim <bob217@mail.ru>
2024-09-02 22:30:07 +0700

    lesson 7

---
+++ JuniorFactory.Lesson7/Controllers/UsersController.cs
@@ -0,0 +1,61 @@
+﻿using JuniorFactory.Lesson7.Models.Users;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using JuniorFactory.Lesson7.Middlewares;
+
+namespace JuniorFactory.Lesson7.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly ILogger<UsersController> _logger;
+
+        public UsersController(ILogger<UsersController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        [Route("Login")]
+        public string Login([FromBody] LoginModel model)
+        {
+            if (model.User == "admin")
+            {
+                if (model.Password == "123")
+                {
+                    return "secret123token";
+                }
+            }
+            throw new Exception("user or password incorrect");
+        }
+
+        [HttpPost]
+        [Route("Username")]
+        [AuthFilter(Permissions.User)]
+        public string GetUsername()
+        {
+            return "Павел";
+        }
+
+        [HttpPost]
+        [Route("Balance")]
+        [AuthFilter(Permissions.Admin)]
+        public string GetBalance()
+        {
+            return "606217рублей";
+        }
+
+        /// <summary>
+        /// Получить дом.
+        /// </summary>
+        /// <param name="id">Айди дома.</param>
+        /// <returns>Инфа о доме.</returns>
+        [HttpPost]
+        [Route("House/{id}")]
+        public string GetHouse(int id)
+        {
+            return "dom number " + id;
+        }
+    }
+}
